7c1cdc1df99ec1bf019edd416fdeb9375230da89
Khalid <khalid@sportjacks.com>
2024-06-30 20:48:41 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // experimental: {
+  //   reactCompiler: true,
+  // },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  // Override the default webpack configuration
+  webpack: (config) => {
+    // See https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      sharp$: false,
+      "onnxruntime-node$": false,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
